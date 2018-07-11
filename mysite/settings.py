e5c98aec6945315f81caac23cd7a304c8fc905e4
joshuaDclark <mrjoshuadclark@gmail.com>
2018-07-11 14:39:41 -0400

    remove django_heroku from settings file

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,5 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-django_heroku.settings(locals())
